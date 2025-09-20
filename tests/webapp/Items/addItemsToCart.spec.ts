475e76c80d46d65b96fb1d85e4419e350eb575f9
MaximeS <maxime.sanschagrin@notarius.com>
2025-09-20 14:39:55 -0400

    initiate item page tests

---
+++ tests/webapp/Items/addItemsToCart.spec.ts
@@ -0,0 +1,8 @@
+import {test, expect} from '@playwright/test';
+
+// define const called in function parameters
+const url = process.env.SAUCE_URL;
+test('add items to cart', async ({page}) => {
+
+
+});
